bda52031ebfffaf328d28d2e878b6ca7c28ffec2
watacus <watacus@googlemail.com>
2017-03-30 11:18:52 +0100

    Add files via upload

--- ChatServer/src/uk/ac/livjm/cms/Chat.java
+++ ChatServer/src/uk/ac/livjm/cms/Chat.java
@@ -29,7 +29,8 @@ public class Chat extends JFrame implements ActionListener {
 	private JTextArea messageText;
 	private JTextArea historyText;
 	private Server server;
-	private JCheckBox urlButton;
+	private JCheckBox urlCheckBox;
+	private JCheckBox saveCheckBox;
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
@@ -42,17 +43,17 @@ public class Chat extends JFrame implements ActionListener {
 			server.getConnection().sendLine(text);
 			historyText.append("Me:\t" + text + "\n");
 			messageText.setText("");
-			
+
 			try {
-				BufferedWriter logWriter = new BufferedWriter (new FileWriter("log.txt", true));
+				BufferedWriter logWriter = new BufferedWriter(new FileWriter("log.txt", true));
 				logWriter.newLine();
 				logWriter.write("Me:\t" + text + "\n");
 				logWriter.close();
 			} catch (Exception f) {
-				
+
 			}
 		}
-}	
+	}
 
 	public static void main(String[] args) {
 
@@ -114,7 +115,8 @@ public class Chat extends JFrame implements ActionListener {
 
 		// Add history
 		constraints = new GridBagConstraints();
-		historyText = new JTextArea("Welcome to Chatter version 1.2!\n Please use the close button to close the chat application.\n");
+		historyText = new JTextArea(
+				"Welcome to Chatter version 1.2!\n Please use the close button to close the chat application.\n");
 		constraints.gridx = 0;
 		constraints.gridy = 1;
 		constraints.weightx = 1.0;
@@ -124,17 +126,27 @@ public class Chat extends JFrame implements ActionListener {
 		constraints.fill = GridBagConstraints.BOTH;
 		historyText.setEditable(false);
 		add(historyText, constraints);
-		
-		//Adds checkbox for automatically opening URLs
+
+		// Adds checkbox for automatically opening URLs
 		constraints = new GridBagConstraints();
-		urlButton = new JCheckBox("Auto open URLs sent in chat");
+		urlCheckBox = new JCheckBox("Auto open URLs sent in chat");
 		constraints.gridx = 2;
-		constraints.gridy = 3;
+		constraints.gridy = 5;
 		constraints.weightx = 0.0;
 		constraints.weighty = 0.0;
-		constraints.anchor = GridBagConstraints.CENTER;
-		add(urlButton, constraints);
-
+		constraints.anchor = GridBagConstraints.FIRST_LINE_END;
+		add(urlCheckBox, constraints);
+		
+		//Adds checkbox for text saving checkbox
+		constraints = new GridBagConstraints();
+		saveCheckBox = new JCheckBox("Save chat history to .txt file");
+		constraints.gridx = 2;
+		constraints.gridy = 5;
+		constraints.weightx = 0.0;
+		constraints.weighty = 0.0;
+		constraints.anchor = GridBagConstraints.FIRST_LINE_START;
+		add(saveCheckBox, constraints);
+		
 		// Send message text
 		constraints = new GridBagConstraints();
 		constraints.gridx = 0;
@@ -151,31 +163,32 @@ public class Chat extends JFrame implements ActionListener {
 		messageText.setLineWrap(true);
 		historyText.setWrapStyleWord(true);
 		messageText.setWrapStyleWord(true);
-		urlButton.setSelected(true);
-
+		urlCheckBox.setSelected(true);
+		saveCheckBox.setSelected(true);
+		
 		server = new Server(this, 7047);
 		Thread thread = new Thread(server);
-		thread.start();}
-	
+		thread.start();
+	}
 
 	void setRecievedText(String text) throws IOException, URISyntaxException {
 
-		try{
-			BufferedWriter logWriter = new BufferedWriter (new FileWriter("log.txt",true)); //Writes recieved text to .txt file
+		try {
+			BufferedWriter logWriter = new BufferedWriter(new FileWriter("log.txt", true)); // Writes received text to .txt file
 			logWriter.newLine();
 			logWriter.write("User:\t" + text + "\n");
 			logWriter.close();
-		} catch (IOException e){
-			
+		} catch (IOException e) {
+
 		}
 		historyText.append("User:\t" + text + "\n");
-		
-		if(urlButton.isSelected()) {
+
+		if (urlCheckBox.isSelected()) { //Checks state of urlButton JCheckBox and runs code if it is selected
 			String checking = text;
 			String checked = checking.replaceAll("http://.+?(com|net|org)/{0,1}", "<a href=\"$0\">$0</a>");
-			if(Desktop.isDesktopSupported()) {
+			if (Desktop.isDesktopSupported()) {
 				Desktop.getDesktop().browse(new URI(checked));
 			}
 		}
-		}
+	}
 }
